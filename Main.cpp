274b9ef6e7673b941dd4e244bb1b05fa26b0947b
Bebennnn <Benaya.gls@gmail.com>
2024-12-30 15:41:51 +0700

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -10,52 +10,50 @@ int main() {
     int pilihan;
 
     do {
-        cout << "\nMENU UTAMA" << endl;
-        cout << "1. Tampilkan Semua Rute" << endl;
-        cout << "2. Tampilkan Gedung dengan Jalan Terbanyak" << endl;
-        cout << "3. Jalur Terpendek (Dijkstra)" << endl;
-        cout << "4. Jalur Alternatif dengan Menghindari Gedung" << endl;
+        cout << "-----MENU UTAMA-----" << endl;
+        cout << "1. Tampilkan Semua Gedung dan Rute" << endl;
+        cout << "2. Tampilkan Gedung dengan Jalur Terbanyak" << endl;
+        cout << "3. Cari Jalur Terpendek" << endl;
+        cout << "4. Cari Jalur Alternatif" << endl;
         cout << "0. Keluar" << endl;
         cout << "Silahkan Pilih Menu: ";
         cin >> pilihan;
 
         switch (pilihan) {
             case 1: {
-                cout << "\nRute yang Tersedia:" << endl;
                 showGraph(G);
                 break;
             }
             case 2: {
-                cout << "\nGedung dengan Jalan Terbanyak:" << endl;
                 printGedungDenganCabangTerbanyak(G);
                 break;
             }
             case 3: {
                 string startID, endID;
-                cout << "\nMasukkan ID Gedung Awal: ";
+                cout << "\nMasukkan Gedung Asal: ";
                 cin >> startID;
-                cout << "Masukkan ID Gedung Tujuan: ";
+                cout << "Masukkan Gedung Tujuan: ";
                 cin >> endID;
                 JalanTerpendek(G, startID, endID);
                 break;
             }
             case 4: {
                 string startID, endID, avoidID;
-                cout << "\nMasukkan ID Gedung Awal: ";
+                cout << "\nMasukkan Gedung Asal: ";
                 cin >> startID;
-                cout << "Masukkan ID Gedung Tujuan: ";
+                cout << "Masukkan Gedung Tujuan: ";
                 cin >> endID;
-                cout << "Masukkan ID Gedung yang Dihindari: ";
+                cout << "Masukkan Gedung yang Dihindari: ";
                 cin >> avoidID;
-                CariJalanAlternatif(G, startID, endID, avoidID); // Menampilkan jalur alternatif
+                CariJalanAlternatif(G, startID, endID, avoidID);
                 break;
             }
             case 0: {
-                cout << "\nKeluar dari aplikasi. Terima kasih!" << endl;
+                cout << "\nKeluar dari program!" << endl;
                 break;
             }
             default: {
-                cout << "\nPilihan Tidak Tersedia. Silakan pilih lagi." << endl;
+                cout << "\nMenu Tidak Tersedia." << endl;
                 break;
             }
         }
